0abdcac04491a1dfeb1324946cd7f5a865c2bb36
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2023-01-13 18:53:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
-const { getPost, addPost } = require('./controller/post')
+const { getPost, addPost, addLike, deletePost } = require('./controller/post')
 require('dotenv').config({ path: './.env' })
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -23,10 +23,11 @@ app.get('/posts', async (req, res) => {
     try {
         const post = await getPost()
         let modificado = post.map((p) => ({
-        id: p.id,
-        titulo: p.titulo,
-        img: p.img,
-        descripcion: p.description,
+            id: p.id,
+            titulo: p.titulo,
+            img: p.img,
+            descripcion: p.description,
+            likes: p.likes,
         }))
         res.json(modificado)
     } catch (error) {
@@ -36,13 +37,42 @@ app.get('/posts', async (req, res) => {
 
 app.post('/posts', async (req, res) => {
     try {
-        const { titulo, url, descripcion, likes } = req.body
+        const likes = 0
+        const { titulo, url, descripcion } = req.body
         console.log(req.body)
 
         await addPost(titulo, url, descripcion, likes)
-        res.send('Post Added successfull')
+        res.send('Post added successfully')
     } catch (error) {
-        res.json({ message: 'This is not available. Come back later!' })
+        res.json({ message: 'This is not available. Come back later! ' })
+    }
+})
+
+app.put('/posts/like/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        await addLike(id)
+        res.status(200).json({
+        message: 'Like added successfully',
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
+})
+
+app.delete('/posts/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        await deletePost(id)
+        res.status(200).json({
+        message: 'Post deleted successfully',
+        })
+    } catch (error) {
+        res.status(500).json({
+        message: error.message,
+        })
     }
 })
 
